f0b7165e8716263291fef9ae417d7fce248e5ea6
Phil Hagelberg <technomancy@gmail.com>
2012-02-16 17:26:25 -0800

    Remove project.clj.

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject s3-wagon-private "1.0.1"
-  :description "Deploy artifacts to private S3 URLs."
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.springframework.build.aws/org.springframework.build.aws.maven
-                  "3.0.0.RELEASE"]
-                 [org.apache.maven.wagon/wagon-provider-api "1.0"]]
-  :resources-path "res"
-  :aot [s3-wagon.private])
